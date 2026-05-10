5616d8210ffb90d9e5bb70463cc760d330a1b510
TendersandMore <jameshewlett96@hotmail.co.uk>
2026-05-10 16:47:30 +0100

    Update tender.js

--- pages/api/tender.js
+++ pages/api/tender.js
@@ -1,12 +1,23 @@
 export default async function handler(req, res) {
-  const params = new URLSearchParams(req.query).toString();
-  const url = `https://www.find-tender.service.gov.uk/api/1.0/ocdsReleasePackages?${params}`;
-
-  const response = await fetch(url, {
-    headers: { Accept: "application/json" }
-  });
-
-  const data = await response.json();
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(response.status).json(data);
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  try {
+    const params = new URLSearchParams(req.query).toString();
+    const url = `https://www.find-tender.service.gov.uk/api/1.0/ocdsReleasePackages?${params}`;
+
+    const response = await fetch(url, {
+      headers: { Accept: "application/json" }
+    });
+
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
